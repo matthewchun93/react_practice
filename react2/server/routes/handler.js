b3309fc78b6db21d6014553703cae7f0e1c53f2c
matthew <matthewchun93@gmail.com>
2021-09-15 18:37:24 -0700

    mass commit

--- react2/server/routes/handler.js
+++ react2/server/routes/handler.js
@@ -19,8 +19,34 @@ router.post('/newitem', (req, res) => {
   .then(item => console.log('Note saved to Database'))
   .catch(err => res.status(400).json("Error: " + err))
 })
+
 //delete route
+router.delete('/delete/:id', (req, res) => {
+  const id = req.params.id;
+  Item.findByIdAndDelete({ _id: id }, (req, res, err) => {
+    if(!err) {
+      console.log("Item deleted");
+    } else {
+      console.log(err)
+    }
+  })
+})
 
 //update route
+router.put('/put/:id', (req, res) => {
+  const updatedItem = {
+    title: req.body.title,
+    description: req.body.description,
+  }
+  const id = req.params.id;
+  Item.findByIdAndUpdate({ _id: id }, {$set: updatedItem}, (req, res, err) => {
+    if (!err) {
+      console.log('Item updated');
+    } else {
+      console.log(err);
+    }
+  })
+})
+
 
 module.exports = router;
