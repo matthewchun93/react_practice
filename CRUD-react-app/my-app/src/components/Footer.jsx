95e0948e54134e91592645d38602be399be8cb31
matthew <matthewchun93@gmail.com>
2021-09-16 13:06:53 -0700

    removed more subfolder git

---
+++ CRUD-react-app/my-app/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <h3>thank &hearts; you</h3>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
