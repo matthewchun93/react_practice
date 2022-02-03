b985fc32597e1c98f7d96f69927905c47bbce9f1
matthew <matthewchun93@gmail.com>
2022-02-03 13:27:30 -0800

    added some styling

--- memory_project/client/src/App.js
+++ memory_project/client/src/App.js
@@ -1,25 +1,42 @@
 import React from "react";
 
 // material UI components
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
 // images
-import memories from './images/memories.png'
+import memories from "./images/memories.png";
+
+// styles
+import useStyles from "./styles";
 
 // components
-import Posts from './components/Posts/Posts.js';
-import Form from './components/Form/Form.js';
+import Posts from "./components/Posts/Posts.js";
+import Form from "./components/Form/Form.js";
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <Container maxwidth='lg'>
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Memories</Typography>
-        <img src={memories} alt="memories" height="60" />
+    <Container maxwidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between"  alignItems="stretch" spacing={3}>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
             <Grid item xs={12} sm={7}>
               <Posts />
             </Grid>
